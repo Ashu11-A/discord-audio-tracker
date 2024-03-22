6822107838920b168bb244c2252fd902f29c12b2
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2024-03-22 16:02:48 +0800

    Tentativas

--- src/class/Voice.ts
+++ src/class/Voice.ts
@@ -1,94 +1,80 @@
-import { OpusEncoder } from '@discordjs/opus'
-import { EndBehaviorType, VoiceConnection } from '@discordjs/voice'
-import { VoiceState } from 'discord.js'
-import { createWriteStream } from 'fs'
-import { pipeline, PassThrough } from 'node:stream'
-import prism from 'prism-media'
-import { io } from 'socket.io-client'
+import { OpusEncoder } from '@discordjs/opus';
+import { EndBehaviorType, VoiceConnection } from '@discordjs/voice';
+import { VoiceState } from 'discord.js';
+import fs, { createWriteStream } from 'fs/promises';
+import { PassThrough } from 'stream';
+import prism from 'prism-media';
+import { io, Socket } from 'socket.io-client';
 import ffmpeg from 'fluent-ffmpeg';
+import { sleep } from '@magicyan/discord';
 
 interface ConstructorProps {
-    state: VoiceState
-    connection: VoiceConnection
+    state: VoiceState;
+    connection: VoiceConnection;
 }
 
 export class VoiceTransmitter {
-    private readonly receiver
-    private readonly speakers
-    private readonly encoder
-    private readonly userId
+    private readonly receiver;
+    private readonly speakers;
+    private readonly encoder;
+    private readonly userId;
 
     constructor({ state, connection }: ConstructorProps) {
-        this.receiver = connection.receiver
-        this.userId = state.id
-        this.speakers = new Set()
-        this.encoder = new OpusEncoder(48000, 2)
+        this.receiver = connection.receiver;
+        this.userId = state.id;
+        this.speakers = new Set();
+        this.encoder = new OpusEncoder(48000, 2);
 
         // Removemos os ouvintes para utilizar a nossa própria função.
-        this.receiver.speaking.removeAllListeners()
-        this.receiver.speaking.on('start', () => this.listen())
-        this.speakers.add(this.userId)
+        this.receiver.speaking.removeAllListeners();
+        this.receiver.speaking.on('start', () => this.listen());
+        this.speakers.add(this.userId);
     }
 
-    async socket() {
+    private async createSocket(): Promise<Socket> {
         return io('http://localhost:3000', {
             transports: ['websocket'],
             upgrade: false
         });
     }
 
-    opusStream() {
+    private opusStream() {
         return this.receiver.subscribe(this.userId, {
             end: {
                 behavior: EndBehaviorType.AfterSilence,
-                duration: 50
+                duration: 5000
             }
-        })
+        });
     }
 
-    async listen() {
-        const passThroughStream = new PassThrough({ allowHalfOpen: false });
-        const socket = await this.socket()
-        const opusStream = this.opusStream()
+    private async listen() {
+        const socket = await this.createSocket();
+        const opusStream = this.opusStream();
 
         const opusDecoder = new prism.opus.Decoder({
             frameSize: 960,
             channels: 2,
             rate: 48000,
-          });
+        });
 
-
-        const filename = `./recordings/${this.userId}_${Date.now()}.pcm`;
-        const outFile = createWriteStream(filename, { flags: 'a' })
+        // const outFile = createWriteStream(filename, { flags: 'a' });
 
         socket.on('connect', () => {
             console.log(`👂 Started Sending audio by ${this.userId}`);
-        })
+        });
 
-        pipeline(opusStream, opusDecoder, outFile, (err) => {
-            if (err) {
-                console.warn(`❌ Error recording file ${filename} - ${err.message}`);
-            } else {
-                console.log(`✅ Recorded ${filename}`);
-            }
-        })
-
-        opusStream.once('data', (chunk) => {
-            ffmpeg(chunk)
-                .inputOptions(['-f s16le', '-ar 48000', '-ac 2'])
-                .outputFormat('mp3')
-                .output(passThroughStream)
-                .run();
-            socket.emit('audioStream', passThroughStream)
+        opusStream.once('data', (chunck) => {
+            console.log(this.encoder.decode(chunck))
+            socket.emit('audioStream', this.encoder.decode(chunck))
         })
 
         opusStream.on('error', (error: Error) => {
             console.error(error, `Error while recording voice for user ${this.userId}`);
         });
 
-        opusStream.once('end', async () => {   
-            socket.disconnect()
-            this.speakers.delete(this.userId)
-        })
+        opusStream.once('end', async () => {
+            socket.disconnect();
+            this.speakers.delete(this.userId);
+        });
     }
 }
